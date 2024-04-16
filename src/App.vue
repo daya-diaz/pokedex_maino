97339d4cd907e7b69072ae5c5ff9258f32929cd0
Dayane Santos <dayadiazprofissional@gmail.com>
2024-04-16 11:15:23 -0300

    limpando projeto e criando componente Header

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <Header></Header>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(
+    to bottom right,
+    rgba(225, 255, 255, 1),
+    rgba(245, 245, 245, 1)
+  )
+  no-repeat center center fixed !important;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <img src="../assets/pokemon-logo.png" alt="" />
+    <nav>
+      <ul>
+        <li><a href="#">Pokedéx</a></li>
+        <li><a href="https://linkedin.com/in/dayane-santos-melo" target="_blank">LinkedIn</a></li>
+        <li><a href="https://github.com/daya-diaz" target="_blank">Github</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #F5DB13;
+  color: #212121;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 6rem; 
+  padding-block: 1rem;
+  width: 100%; 
+  filter: drop-shadow(0px 3px 8px #cccccc);
+
+  @media (max-width: 768px) {
+    padding-inline: 1rem; 
+  }
+
+  nav {
+    ul {
+      list-style: none;
+      padding: 0;
+      display: flex;
+
+      li {
+        margin-right: 1rem;
+
+        &:last-child {
+          margin-right: 0; 
+        }
+
+        a {
+          color: #212121;
+          font-size: 1.2rem;
+          text-decoration: none;
+
+          @media (max-width: 768px) {
+            font-size: 1rem;
+          }
+        }
+      }
+    }
+  }
+}
+
+</style>
