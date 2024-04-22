dea83a1172389c208bb3cc6b3ce855c34f8a9a57
Dayane Santos <dayadiazprofissional@gmail.com>
2024-04-22 14:24:11 -0300

    mostrando 300 pokemons em tela.

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -57,7 +57,7 @@ export default {
     }
   },
   mounted() {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=10").then((response) => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=300").then((response) => {
       this.pokemons = response.data.results;
 
       this.pokemons.forEach(pokemon => {
